696769f1934011a96caf7f95b44aa79e73939636
unknown <mariascofa@gmail.com>
2023-03-18 09:06:14 +0000

    updated logs

--- hmwrk_6.js/hmw_6.js
+++ hmwrk_6.js/hmw_6.js
@@ -1,9 +1,11 @@
-// 1 Find the sum and the amount of positive elements in the array.
-
 const numbersArray = [
   16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
   76, -4, 12, -35, 4, 47,
 ];
+
+console.log(numbersArray);
+
+// 1 Find the sum and the amount of positive elements in the array.
 
 let countNumb = 0;
 let sumNumb = 0;
@@ -15,33 +17,42 @@ numbersArray.forEach(function (element) {
 });
 
 console.log(
-  `Amount of positive numbers - ${countNumb}.\nSum of all postive numbers - ${sumNumb}.`
+  `Amount of positive numbers: ${countNumb}.\nSum of all postive numbers: ${sumNumb}.`
 );
 
 // 2 Find the minimum value and its sequence number in the array.
 
 const minValue = Math.min(...numbersArray);
 
-let minIndex =
-  numbersArray.findIndex(function (item) {
-    return item === minValue;
-  }) + 1;
-console.log(minValue, minIndex);
+let minIndex = numbersArray.findIndex(function (item) {
+  return item === minValue;
+});
+
+console.log(
+  `Minimum value: ${minValue}.\nIndex of the minimum number: ${minIndex}. Sequence number: ${
+    minIndex + 1
+  }.`
+);
 
 // 3 Find the biggest value and its sequence number in the array.
 
 let maxElement = numbersArray[0];
-let maxIndex = 1;
+let maxIndex = 0;
 
-for (let index = 1; index < numbersArray.length; index++) {
+for (let index = 0; index < numbersArray.length; index++) {
   if (numbersArray[index] > maxElement) {
     maxElement = numbersArray[index];
-    maxIndex = index + 1;
+    maxIndex = index;
   }
 }
-console.log(maxElement, maxIndex);
 
-// 4 Determine the number of negative values.
+console.log(
+  `Maximum value: ${maxElement}.\nIndex of the maximum number: ${maxIndex}. Sequence number: ${
+    maxIndex + 1
+  }.`
+);
+
+// 4 Find the amount of negative values.
 
 let countNegativeNumb = 0;
 
@@ -51,21 +62,19 @@ numbersArray.forEach(function (element) {
   }
 });
 
-console.log(countNegativeNumb);
+console.log(`Amount of negative values in the array: ${countNegativeNumb}.`);
 
 // 5 Find the amount of uneven positive elements.
 
 let countPositiveNumb = 0;
-let sumPositiveNumb = 0;
 
 numbersArray.forEach(function (element) {
   if (element > 0 && element % 2 !== 0) {
     countPositiveNumb++;
-    sumPositiveNumb += element;
   }
 });
 
-console.log(countPositiveNumb, sumPositiveNumb);
+console.log(`Amount of of uneven positive elements: ${countPositiveNumb}.`);
 
 // 6 Find the amount of even positive elements.
 
@@ -73,8 +82,9 @@ const countPositiveEvenNumb = numbersArray.filter(function (element) {
   return element > 0 && element % 2 === 0;
 });
 
-console.log(countPositiveEvenNumb.length);
-console.log(countPositiveEvenNumb);
+console.log(
+  `Amount of even positive elements in the array: ${countPositiveEvenNumb.length}.`
+);
 
 // 7 Find the sum of all positive and even elements.
 
@@ -84,19 +94,19 @@ const sumPositiveEven = numbersArray
   })
   .reduce((sum, currentValue) => sum + currentValue);
 
-console.log(sumPositiveEven);
+console.log(`Sum of all positive and even elements: ${sumPositiveEven}.`);
 
 //8 Find the sum of all positive and uneven elements.
 
-let countingNumb = 0;
+let positiveUnevenSum = 0;
 
 numbersArray.forEach(function (element) {
   if (element > 0 && element % 2 !== 0) {
-    countingNumb += element;
+    positiveUnevenSum += element;
   }
 });
 
-console.log(countingNumb);
+console.log(`Sum of all positive and uneven elements: ${positiveUnevenSum}.`);
 
 //9 Find the product of all positive values in the array.
 
@@ -106,17 +116,21 @@ const productPositive = numbersArray
   })
   .reduce((product, eachValue) => product * eachValue);
 
-console.log(productPositive);
+console.log(
+  `The product of all positive values in the array: ${productPositive}.`
+);
 
-//10 Find the biggest value, other values change to zero.
+//10 Find the biggest value in the array, other values change to zero.
 
 const maxValue = Math.max(...numbersArray);
 
-const UpdateArray = numbersArray.map(function (element) {
+const UpdatedArray = numbersArray.map(function (element) {
   return element === maxValue ? element : 0;
 });
 
-console.log(UpdateArray);
+console.log(
+  `New array, with the numbers, that are not equal to maximum, updated to zero: ${UpdatedArray}.`
+);
 
 for (let newIndex = 0; newIndex < numbersArray.length; newIndex++) {
   if (numbersArray[newIndex] !== maxValue) {
@@ -124,4 +138,4 @@ for (let newIndex = 0; newIndex < numbersArray.length; newIndex++) {
   }
 }
 
-console.log(numbersArray);
+console.log(`Initial array with the updated values: ${numbersArray}.`);
